caefc5ded8421ae029c9eb629b982253c7c0b224
Paulo César dos Passos Pereira <paulo.passos.pereira1@outlook.com>
2026-02-25 18:09:42 -0300

    criacao do projeto

---
+++ api_assinaturas/src/main/java/br/com/passos/api_assinaturas/ApiAssinaturasApplication.java
@@ -0,0 +1,13 @@
+package br.com.passos.api_assinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssinaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAssinaturasApplication.class, args);
+	}
+
+}
---
+++ api_assinaturas/src/test/java/br/com/passos/api_assinaturas/ApiAssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.passos.api_assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
